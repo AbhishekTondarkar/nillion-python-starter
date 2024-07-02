a0716b3ad29b7368d85a8c7ea7f7b3e18387cb8a
AbhishekTondarkar <tondarkarabhi7@gmail.com>
2024-07-02 11:00:38 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,25 @@
 from nada_dsl import *
 
-
 def nada_main():
-    party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
-
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
-
-    return [Output(my_int1, "my_output", party1)]
+    alice = Party(name="Alice")
+    bob = Party(name="Bob")
+    
+    alice_cats = SecretInteger(Input(name="alice_cats", party=alice))
+    bob_dogs = SecretInteger(Input(name="bob_dogs", party=bob))
+    
+    pet_difference = alice_cats - bob_dogs
+    cat_dog_sum = alice_cats + bob_dogs
+    pet_product = alice_cats * bob_dogs
+    
+    cats_win = pet_difference > 0
+    winner = cats_win.if_else("Cats rule!", "Dogs drool!")
+    
+    treat_budget = cat_dog_sum * 5
+    
+    chaos_factor = pet_product / 10
+    
+    return [
+        Output(winner, "contest_result", alice),
+        Output(treat_budget, "treat_budget", bob),
+        Output(chaos_factor, "chaos_factor", alice)
+    ]
